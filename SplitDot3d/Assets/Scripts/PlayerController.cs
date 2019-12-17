7e10666d108adcb3cef16fa84e56928516298f22
TerentevVlad <42178405+TerentevVlad@users.noreply.github.com>
2019-12-17 16:59:57 +0300

    create project

---
+++ SplitDot3d/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public static bool listenTouch = true;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ClickListener();
+    }
+
+    void ClickListener()
+    {
+        if (!listenTouch)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(GameManager.startLvl)
+            {
+                GameManager.startLvl = false;
+                PoolSpheres.MoveFirst();
+            }
+            else if(PoolSpheres.currentObject > 0)
+            {
+
+            }
+        }
+
+    }
+    
+}
